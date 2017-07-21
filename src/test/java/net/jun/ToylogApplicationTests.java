e9a08c58b12cbcfa8cadff7ae71f6944fcf24bad
Young Jun Park <june0313@gmail.com>
2017-07-22 00:19:55 +0900

    initial commit

---
+++ src/test/java/net/jun/ToylogApplicationTests.java
@@ -0,0 +1,16 @@
+package net.jun;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ToylogApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
