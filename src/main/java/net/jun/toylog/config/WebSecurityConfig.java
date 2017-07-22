b7fe4e8c1a0f5a3fc0a2bb017a000d62300dfd67
Young Jun Park <june0313@gmail.com>
2017-07-22 20:51:37 +0900

    시큐리티 기본 설정 : 모든 요청에 대해 인증을 수행하지 않도록 함

---
+++ src/main/java/net/jun/toylog/config/WebSecurityConfig.java
@@ -0,0 +1,24 @@
+package net.jun.toylog.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by wayne on 2017. 7. 22..
+ *
+ */
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests()
+			.anyRequest()
+			.permitAll();
+	}
+
+}
